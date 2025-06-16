a972e3ae299d9e17fe629aa8287d97330d600d25
BragaMPedro <pedrobmagalhaes95@gmail.com>
2025-06-16 17:59:21 -0300

    refactor/stats logic improvements

--- backend/src/utils/stats.js
+++ backend/src/utils/stats.js
@@ -1,6 +1,14 @@
-// Utility intentionally unused by routes (candidate should refactor)
-function mean(arr) {
-  return arr.reduce((a, b) => a + b, 0) / arr.length;
+/**
+ * Iterates the items of an Array and returns the mean of the selected item prop
+ * 
+ * @function mean
+ * @param {any[]} arr - Array having items for the calculations
+ * @param {string} prop - Item property to iterate
+ * @returns {number} Mean of the iterated array item property
+ */
+function mean(arr, prop) {
+
+  return arr.reduce((acc, item) => acc + item[prop], 0) / arr.length;
 }
 
 module.exports = { mean };
