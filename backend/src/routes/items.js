bc705abacdc8d33d06ccd2d4ad7f58ed23352815
BragaMPedro <pedrobmagalhaes95@gmail.com>
2025-06-16 18:04:52 -0300

    refactor/implements utils modules

--- backend/src/routes/items.js
+++ backend/src/routes/items.js
@@ -1,42 +1,6 @@
 const express = require('express');
-const fs = require('fs').promises;
-const path = require('path');
 const router = express.Router();
-const DATA_PATH = path.join(__dirname, '../../../data/items.json');
-
-/**
- * @typedef {object} Item
- * @property {number} id - Unique identifier.
- * @property {string} name - Items name.
- * @property {string} category - Items category.
- * @property {number} price - Items price.
- */
-
-/**
- * Asynchronously reads data from a specified file path and parses it as JSON.
- *
- * @function readData
- * @async
- * @returns {Promise<Item[]>} A promise that resolves with the JSON parsed Item.
- * @throws {Error} Throws an error providing the errors stack trace.
- */
-async function readData() {
-  try {
-    const data = await fs.readFile(DATA_PATH, 'utf-8');
-    return JSON.parse(data);
-  } catch (error) {
-    throw new Error(error.stack);
-  }
-}
-
-async function writeData(data) {
-  try {
-    await fs.writeFile(DATA_PATH, JSON.stringify(data, null, 2), 'utf8');
-
-  } catch (error) {
-    throw new Error(error.stack);
-  }
-}
+const items = require('../utils/items')
 
 /**
  * Formats a given string by removing diacritics (accents), converting it to lowercase,
@@ -57,10 +21,11 @@ function formatString(str) {
 router.get('/', async (req, res, next) => {
 
   try {
-    const data = await readData();
+    const data = await items.readData();
     const { limit, q } = req.query;
     let results = data;
 
+    //Query Logic
     if (q) {
       const query = formatString(q)
 
@@ -70,8 +35,11 @@ router.get('/', async (req, res, next) => {
       });
     }
 
+    //Limit logic default = 20;
     if (limit) {
       results = results.slice(0, parseInt(limit));
+    } else {
+      results = results.slice(0, 20);
     }
     
     res.json(results);
@@ -83,7 +51,7 @@ router.get('/', async (req, res, next) => {
 // GET /api/items/:id
 router.get('/:id', async (req, res, next) => {
   try {
-    const data = await readData();
+    const data = await items.readData();
     const item = data.find(i => i.id === parseInt(req.params.id));
     if (!item) {
       const err = new Error('Item not found');
@@ -101,10 +69,10 @@ router.post('/', async (req, res, next) => {
   try {
     // TODO: Validate payload (intentional omission)
     const item = req.body;
-    const data = await readData();
+    const data = await items.readData();
     item.id = Date.now();
     data.push(item);
-    await writeData(data)
+    await items.writeData(data)
     res.status(201).json(item);
   } catch (err) {
     next(err);
